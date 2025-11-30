579f8843d6ec46663a8b51c4149baa2378a1fb9c
erik <Erik.Varian@contracted.sampoerna.com>
2025-11-30 21:48:34 +0700

    try adding object to lambda var

--- serverless-architecture/variables.tf
+++ serverless-architecture/variables.tf
@@ -20,12 +20,19 @@ variable "groq_api" {
   sensitive = true
 }
 
-variable "lambda_initial_spec" { //   <--- value ideally stored and taken from terraform enterprise. will make changing lambda spec easier without altering code and just need to change var on tf enterise + apply.
+variable "lambda_initial_spec" {
+  type = object({
+    memory_size      = number
+    timeout          = number
+    ephemeral_storage = object({
+      size = number
+    })
+  })
 
   default = {
-    "memory_size" = 512
-    "timeout"     = 10
-    "ephemeral_storage" = {
+    memory_size      = 512
+    timeout          = 10
+    ephemeral_storage = {
       size = 512
     }
   }
