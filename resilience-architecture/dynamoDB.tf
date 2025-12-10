f45a0f676fad20db086a2d2dadb9194c021a6fcc
erik <Erik.Varian@contracted.sampoerna.com>
2025-12-10 21:29:58 +0700

    adding db.tf and add function.py

---
+++ resilience-architecture/dynamoDB.tf
@@ -0,0 +1,16 @@
+resource "aws_dynamodb_table" "lambda_invocation_counter" {
+  name         = "${var.env}-${var.project_name}-lambda-counter"
+  billing_mode = "PAY_PER_REQUEST"  
+
+  hash_key = "pk"
+
+  attribute {
+    name = "pk"
+    type = "S"
+  }
+
+    point_in_time_recovery {
+    enabled = true
+  }
+  tags = local.common_tags
+}
